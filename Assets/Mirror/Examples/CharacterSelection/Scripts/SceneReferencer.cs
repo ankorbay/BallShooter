b3784a01182d5a4fd9fb05ddd096b167946b1d48
ankorbay <41270992+ankorbay@users.noreply.github.com>
2024-10-20 20:26:55 +0300

    Hide ColorSelection button when game starts

--- Assets/Mirror/Examples/CharacterSelection/Scripts/SceneReferencer.cs
+++ Assets/Mirror/Examples/CharacterSelection/Scripts/SceneReferencer.cs
@@ -3,15 +3,26 @@ using UnityEngine.UI;
 
 namespace Mirror.Examples.CharacterSelection
 {
-    public class SceneReferencer : MonoBehaviour
+    public class SceneReferencer : NetworkBehaviour
     {
-        // Make sure to attach these Buttons in the Inspector
         public Button buttonCharacterSelection;
 
         private CharacterData characterData;
         public GameObject characterSelectionObject;
         public GameObject lobbyUIObject;
         public GameObject environment;
+
+        public override void OnStartClient()
+        {
+            base.OnStartClient();
+            gameObject.SetActive(false);
+        }
+
+        public override void OnStopClient()
+        {
+            base.OnStopClient();
+            gameObject.SetActive(true);
+        }
 
         private void Start()
         {
