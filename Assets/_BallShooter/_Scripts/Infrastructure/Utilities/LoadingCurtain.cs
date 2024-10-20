ec63ee837964cc5fb4b1c9b53b93ed435610eb84
ankorbay <41270992+ankorbay@users.noreply.github.com>
2024-10-20 21:46:14 +0300

    Add LoadingCurtain prefab

--- Assets/_BallShooter/_Scripts/Infrastructure/Utilities/LoadingCurtain.cs
+++ Assets/_BallShooter/_Scripts/Infrastructure/Utilities/LoadingCurtain.cs
@@ -1,44 +1,26 @@
-using DG.Tweening;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Logic
 {
     public class LoadingCurtain : MonoBehaviour
     {
         public CanvasGroup curtain;
-        public Image progressFilledImage;
 
-        public float progressFillingDuration = 1f;
-
-        public void Show(bool isAutoHide = true)
+        public void Show()
         {
             if(gameObject.activeSelf == false)
                 gameObject.SetActive(true);
             Debug.Log("Show");
             curtain.alpha = 1;
-            MoveProgress(isAutoHide);
         }
 
-        public void Hide() =>
-            FadeIn();
+        public void Hide()
+        { 
+            Debug.Log("Hide");
+            curtain.alpha = 0;
+        }
 
         private void Awake() => 
             DontDestroyOnLoad(this);
-
-        private void MoveProgress(bool isAutoHide)
-        {
-            if (isAutoHide)
-                progressFilledImage.DOFillAmount(1f, progressFillingDuration).OnComplete(FadeIn);
-            else
-                progressFilledImage.DOFillAmount(1f, progressFillingDuration);
-        }
-
-        private void FadeIn()
-        {
-            Debug.Log("Hide");
-            curtain.alpha = 0;
-            progressFilledImage.fillAmount = 0f;
-        }
     }
 }
