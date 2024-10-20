b3a34bde57b27877928f00271c11b5c10e2ab336
ankorbay <41270992+ankorbay@users.noreply.github.com>
2024-10-21 00:17:50 +0300

    Create SceneNames static class

---
+++ Assets/_BallShooter/_Scripts/Infrastructure/SceneNames.cs
@@ -0,0 +1,9 @@
+﻿namespace _BallShooter._Scripts.Infrastructure
+{
+    public static class SceneNames
+    {
+        public const string Initial = "Initial";
+        public const string ColorSelect = "ColorSelection";
+        public const string LobbyGame = "LobbyGame";
+    }
+}
