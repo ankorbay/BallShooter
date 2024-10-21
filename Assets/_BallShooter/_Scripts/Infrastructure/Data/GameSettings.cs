7519b33e579ee79387a0cee1004f63bb51e3b92a
ankorbay <41270992+ankorbay@users.noreply.github.com>
2024-10-21 22:55:43 +0300

    Add GameSettings tooltips

--- Assets/_BallShooter/_Scripts/Infrastructure/Data/GameSettings.cs
+++ Assets/_BallShooter/_Scripts/Infrastructure/Data/GameSettings.cs
@@ -22,14 +22,18 @@ namespace _BallShooter._Scripts.Infrastructure.Data
     {
         public float speed;
         public float rotationSpeed;
+        public float moveXLimit;
     }
 
     [Serializable]
     public class ShootingSettings
     {
+        [Tooltip("The default force that will be applied to the ball when it is shot. Recommended value to start: 1000.")]
         public float defaultForce;
+        [Tooltip("The minimum force multiplier that will be applied to the ball when it is shot. This is used to prevent the ball from being shot with a force of 0. E.g. if defaultForce is 1000 and minForceMultiplier is 0.5, the ball will be added a force of 500 when shot happens.")]
         public float minForceMultiplier;
-        public float maxForceMultiplier;
+        [Tooltip("Time in seconds that the player has to hold the shoot button to reach the maximum force. Also used as a maxForceMultiplier.")]
         public float forceChargingDuration;
+        public float ballLifeTime;
     }
 }
