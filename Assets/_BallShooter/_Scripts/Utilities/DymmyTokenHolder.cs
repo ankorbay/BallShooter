6de9b4a3cc45ae6067825c6364f72aec2d715086
ankorbay <41270992+ankorbay@users.noreply.github.com>
2024-10-21 23:01:11 +0300

    Add dummy Edgegap API token to NetworkManager prefab
    It could be used to create new EdgeGap service in EdgegapLobbyKcpTransport Editor menu (Create & Deploy Lobby) in case of Lobby error "Can't resolve desination host"

---
+++ Assets/_BallShooter/_Scripts/Utilities/DymmyTokenHolder.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DymmyTokenHolder : MonoBehaviour
+{
+    public string edgegapApiKey = "token 8db9bddc-9185-43e7-9861-fb9a62d03acf";
+}
