1cd5013987ae08cfc0a248b9a1a124e46a202718
ankorbay <41270992+ankorbay@users.noreply.github.com>
2024-10-21 22:53:52 +0300

    Remove old script

--- Assets/_BallShooter/_Scripts/UI/PlayerColorButton.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.UI;
-
-[System.Serializable]
-public class PlayerColorButton
-{
-    public Button button;  // Reference to the button UI// Color that this button represents
-    public Outline outline;  // Outline component to highlight the button
-}
